a134e2a3d9d20f167e6d6fba38ede367c534e268
jjfork <jakubiakrjulian@gmail.com>
2023-03-29 01:20:59 +0200

    added method to generate arrays

--- helpers/generating_arrays.py
+++ helpers/generating_arrays.py
@@ -7,17 +7,17 @@ minimum_number = 0
 maximum_number = 5_000
 
 
-def generate_ascending_array(size_of_array):
+def generate_ascending_array(minimum_number, maximum_number,size_of_array):
     return numpy.arange(minimum_number, maximum_number, size_of_array)
 
 
-def generate_descending_array(size_of_array):
+def generate_descending_array(minimum_number, maximum_number,size_of_array):
     return numpy.arange(minimum_number, maximum_number, size_of_array)[::-1]
 
 
-def random_array(size_of_array):
+def random_array(minimum_number, maximum_number,size_of_array):
     return numpy.random.randint(minimum_number, maximum_number, size_of_array)
 
 
-def same_numbers_array(size_of_array):
+def same_numbers_array(minimum_number, maximum_number,size_of_array):
     return numpy.full(size_of_array, random.randint(minimum_number, maximum_number))
